3576182f1766579c9c502309f8e897abaa67df19
Rajeev Kumar Singh <rajeevs@flock.co>
2018-10-01 12:42:48 +0530

    Frontend Project Setup

---
+++ react-social/src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'http://localhost:8080';
+export const ACCESS_TOKEN = 'accessToken';
---
+++ react-social/src/util/APIUtils.js
@@ -0,0 +1,24 @@
+import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
+
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+    
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response => 
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
