a7bb0e86b519e89f364dcfd579804016fc410e64
Rajeev Kumar Singh <rajeevs@flock.co>
2018-10-01 11:28:42 +0530

    cleanup

--- spring-social/src/test/java/com/example/springsocial/SpringSocialDemoApplicationTests.java
+++ spring-social/src/test/java/com/example/springsocial/SpringSocialDemoApplicationTests.java
@@ -3,24 +3,12 @@ package com.example.springsocial;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RunWith(SpringRunner.class)
 public class SpringSocialDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
-
-	@Test
-	public void testUriComponentsBuilder() {
-		UriComponents uri = UriComponentsBuilder.fromUriString("http://google.com/oauth2Redirect")
-				.queryParam("token", "123")
-				.build();
-		System.out.println(uri.getScheme());
-		System.out.println(uri.getHost());
-		System.out.println(uri);
 	}
 
 }
