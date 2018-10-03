cb312a1ca6bd3f984e04393d00234baa7f4a60f3
Rajeev Kumar Singh <rajeevs@flock.co>
2018-10-03 12:41:08 +0530

    Added CORS config

---
+++ spring-social/src/main/java/com/example/springsocial/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.springsocial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(MAX_AGE_SECS);
+    }
+}
