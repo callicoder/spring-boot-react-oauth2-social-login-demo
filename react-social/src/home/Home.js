18cb0ab72ae0d7686f22fe985bf466df6016c303
Rajeev Kumar Singh <rajeevs@flock.co>
2018-10-04 23:27:16 +0530

    Home page UI

--- react-social/src/home/Home.js
+++ react-social/src/home/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import oAuth2Logo from '../img/oauth2-logo.png';
+import './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div className="home-container">
                 <div className="container">
-                    Spring Boot React OAuth2 Social Login Demo App
+                    <img src={oAuth2Logo} height={300} />
+                    <h1>Spring Boot React OAuth2 Social Login Demo</h1>
                 </div>
             </div>
         )
