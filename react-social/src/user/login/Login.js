5ec139f78adb3f5556396a155a4e7aad65e27681
Rajeev Kumar Singh <rajeevs@flock.co>
2018-10-05 18:00:33 +0530

    reduced timeout

--- react-social/src/user/login/Login.js
+++ react-social/src/user/login/Login.js
@@ -19,7 +19,7 @@ class Login extends Component {
                     pathname: this.props.location.pathname,
                     state: {}
                 });
-            }, 300);
+            }, 100);
         }
     }
     
