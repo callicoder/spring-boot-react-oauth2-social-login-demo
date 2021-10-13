6047060565a82e0b41b3ce9bd9c129e8537fe0f5
Rajeev Kumar Singh <callicoder@gmail.com>
2021-10-13 16:32:50 +0530

    Bump up Sprint Boot version to 2.5.5

--- spring-social/src/test/java/com/example/springsocial/SpringSocialApplicationTests.java
+++ spring-social/src/test/java/com/example/springsocial/SpringSocialApplicationTests.java
@@ -1,10 +1,9 @@
 package com.example.springsocial;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SpringSocialApplicationTests {
 
 	@Test
